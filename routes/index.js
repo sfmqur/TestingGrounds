c034faa49b1469e125a922b0a4d0f3f7c0113c82
sfmqur <sammaruska@gmail.com>
2015-10-08 11:12:19 -0500

    created threejstest

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+//threejstest
+router.get('/threejstest', function(req, res) {
+  res.render('threejstest/index');
+});
+
+router.get('/threejstest/firstcube', function(req, res) {
+  res.render('threejstest/firstcube');
+});
+
 
 module.exports = router;
