bc5f4f4cc56db32e2014caf58287cc1a2658ba84
sfmqur <sammaruska@gmail.com>
2015-10-09 14:41:18 -0500

    added lighting to firstcube

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Testing Grounds' });
 });
 
-//threejstest
+//get threejstest
 router.get('/threejstest', function(req, res) {
   res.render('threejstest/index');
 });
